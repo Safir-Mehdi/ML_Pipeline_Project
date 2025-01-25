61dfba2c747d26fb3993e2da1a4b19c10889dea3
Safir-Mehdi <safir816@gmail.com>
2025-01-26 00:53:20 +0500

    EDA section has been completed

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import seaborn as sns
 from src.exception import CustomException
 from src.logger import logging
-from typing import Union
+from typing import Union, Optional
 from scipy.stats.mstats import winsorize
 from sklearn.base import BaseEstimator, TransformerMixin
 from matplotlib import pyplot as plt
@@ -86,7 +86,9 @@ class Winsorizer(BaseTransformer):
 def plot_categorical_features(
     data,
     columns,
-    target_col,
+    # target_col: Optional[str] = None,
+    fixed_hue: Optional[str] = None,
+    orientation: str = 'v',
     plot_type: str = 'countplot',
     x_axis_label: Union[str, list, np.ndarray] = '',
     y_axis_label: Union[str, list, np.ndarray] = 'Count',
@@ -94,41 +96,7 @@ def plot_categorical_features(
     main_title: str = 'Figure Title',
     **kwargs
 ):
-    '''
-    Plot the categorical features against the target column using the specified plot type.
-    
-    Parameters:
-    -----------
-    - data: pd.DataFrame
-        The dataset containing the features and target column.
-        
-    - columns: list
-        List of categorical features to plot.
-        
-    - target_col: str
-        The target column (e.g., 'income').
-        
-    - plot_type: str
-        Type of seaborn plot to use (e.g., 'countplot', 'boxplot', etc).
-        
-    - x_axis_label: str
-        To set the x-axis label.
-    
-    - y_axis_label: str
-        To set the y-axis label.
-        
-    - subplot_title: str
-        To set the axis title.
-    
-    - main_title: str
-        To set the figure title.
-        
-    - **kwargs: Additional keyword arguments to pass to the seaborn plot function.
-    
-    Return:
-    -------
-    - None
-    '''
+
     # Convert a single string into a list for compatibility 
     if isinstance(x_axis_label, str):
         x_axis_label = [x_axis_label] * len(columns)
@@ -136,6 +104,10 @@ def plot_categorical_features(
         y_axis_label = [y_axis_label] * len(columns)
     if isinstance(subplot_title, str):
         subplot_title = [subplot_title] * len(columns)
+        
+    # Validate orientation input
+    if orientation not in ['v', 'h']:
+        raise ValueError('Orientation must be either "v" (vertical) or "h" (horizontal)')
     
     # Number of subplots
     num_col = len(columns)
@@ -145,6 +117,7 @@ def plot_categorical_features(
     # Create subplots
     fig, axes = plt.subplots(rows, cols, figsize=(15, rows * 5))
     axes = axes.flatten()
+    
     
     for i, feature in enumerate(columns):
         # Dynamically call the appropriate seaborn plot function
@@ -161,14 +134,26 @@ def plot_categorical_features(
                 valid_kwargs['order'] = data[feature].value_counts().index
                 # print(valid_kwargs['order'])
         
-        plot_func(
-            data=data,
-            x=feature,
-            # y=target_col,
-            hue=feature,
-            ax=axes[i],
-            **valid_kwargs  # Pass only valid kwargs
-        )
+        hue_param = fixed_hue or feature
+        
+        if orientation == 'v':
+            plot_func(
+                data=data,
+                x=feature,
+                # y=target_col,
+                hue=hue_param,
+                ax=axes[i],
+                **valid_kwargs  # Pass only valid kwargs
+            )
+        
+        elif orientation == 'h':
+            plot_func(
+                data=data,
+                y=feature,
+                hue=hue_param,
+                ax=axes[i],
+                **valid_kwargs  # Pass only valid kwargs
+            )
         
         # Set title and labels
         axes[i].set_title(subplot_title[i].title(), fontsize=14, fontweight='light')
